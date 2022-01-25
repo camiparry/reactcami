b142c8ad4b849a88ee04cad74f3179ab55126736
Molina Hoffmann <A445798@santander.com.ar>
2022-01-24 23:13:51 -0300

    practica

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+const NavBar = () => {
+return(
+    <nav className= "Navigation">
+        <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Galería</li>
+            <li>Contacto</li>
+
+        </ul>
+    </nav>
+);
+};
+
+export default NavBar;
